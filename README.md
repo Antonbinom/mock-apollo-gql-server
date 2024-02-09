a0855f2260eb3a281dcb7dc9e99bd027d0031896
Antonbinom <protest_the_hero@mail.ru>
2024-02-09 15:16:27 +0530

    add: readme

---
+++ README.md
@@ -0,0 +1,13 @@
+# Yat fake server
+
+## Install the dependencies
+
+```bash
+npm i
+```
+
+## Run fake server
+
+```bash
+npm run start
+```
--- index.ts
+++ index.ts
@@ -1,10 +1,16 @@
 import { ApolloServer } from "apollo-server";
 import typeDefs from "./src/schema";
 import resolvers from "./src/resolvers";
+import { config } from "dotenv";
+config();
+
+const port = process.env.PORT;
 
 const server = new ApolloServer({
   typeDefs,
   resolvers,
 });
 
-server.listen().then(({ url }) => console.log(`Server Running on port ${url}`));
+server
+  .listen({ port })
+  .then(({ url }) => console.log(`Server Running on port ${url}`));
